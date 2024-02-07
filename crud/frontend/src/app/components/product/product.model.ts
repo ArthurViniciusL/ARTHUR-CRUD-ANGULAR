a6c75884835f97aad28640853eeb59002e96fe34
ArthurViniciusSL <76112437+ArthurViniciusL@users.noreply.github.com>
2024-02-07 20:40:52 -0300

    Customização do pop-up + id numérico

--- crud/frontend/src/app/components/product/product.model.ts
+++ crud/frontend/src/app/components/product/product.model.ts
@@ -4,4 +4,3 @@ export interface Product {
     name: string;
     price: number;
 }
-
--- crud/frontend/src/app/components/product/product.service.ts
+++ crud/frontend/src/app/components/product/product.service.ts
@@ -14,30 +14,29 @@ export class ProductService {
 
   BASE_URL = 'http://localhost:3001/products';
 
+  // , 
   showMessage(msg: string, isError: boolean = false): void {
-    this.snackBar.open(msg, 'x', {
+    this.snackBar.open(msg, 'ⓧ', {
       duration: 30000,
-      horizontalPosition: "right",
-      verticalPosition: "top",
-      panelClass: isError ? ['msg-error'] : ['msg-success']
+      verticalPosition: "bottom",
+      horizontalPosition: "end"
     });
   }
 
   create(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.BASE_URL, product).pipe(map(obj => obj),catchError(e => this.errorLoad(e)));
+    return this.http.post<Product>(this.BASE_URL, product).pipe(map(obj => obj), catchError(e => this.errorLoad(e)));
   }
 
-  errorLoad(e: any):Observable<any> {
+  errorLoad(e: any): Observable<any> {
     console.log(e);
     this.showMessage("Ocorreu um error não tratado!", true)
     return EMPTY;
   }
 
-
   read(): Observable<Product[]> {
     return this.http.get<Product[]>(this.BASE_URL);
   }
-  
+
   readById(id: string): Observable<Product> {
     const ID_URL = `${this.BASE_URL}/${id}`;
     return this.http.get<Product>(ID_URL);
